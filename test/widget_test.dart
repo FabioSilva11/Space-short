93b821bb9c5be284388f8526233c0aebb1f42f25
Fabio Silva <109297324+FabioSilva11@users.noreply.github.com>
2026-07-01 04:31:05 -0400

    Mock SharedPreferences in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:space_short/src/app/space_short_app.dart';
 
 void main() {
   testWidgets('Space Short menu loads', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const SpaceShortApp());
     await tester.pump();
 
